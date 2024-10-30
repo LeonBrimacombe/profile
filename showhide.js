9c1e22ef1316a865c3334fe4e7d92f8a04f5cddb
Leon Brimacombe <olibrimacombe@gmail.com>
2024-10-30 16:52:26 +0000

    finished

---
+++ showhide.js
@@ -0,0 +1,12 @@
+function showContact() {
+  const x = document.getElementById("hidden-contact");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+/* function showContact(){
+  document.getElementById('hidden-contact').style.display = "block";
+}
+ */
